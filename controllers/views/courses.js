6aae91f89ac2cf32f6b1861d97656fad9ff66abd
Trey Eckels <trey.eckels@gmail.com>
2021-10-08 19:12:21 -0400

    Courses partials.

--- controllers/views/courses.js
+++ controllers/views/courses.js
@@ -2,8 +2,26 @@ const router = require("express").Router();
 // const { Project, User } = require("../models");
 const withAuth = require("../../utils/auth");
 
-router.get("/:id", async (req, res) => {
-  res.render("courses/course");
+router.get("/:id/:content", async (req, res) => {
+  let isDiscussion = false;
+  let isLiveDiscussion = false;
+  let isLiveVideoDiscussion = false;
+
+  switch (req.params.content) {
+    case "discussion":
+      isDiscussion = true;
+      break;
+    case "live-discussion":
+      isLiveDiscussion = true;
+      break;
+    case "live-video-discussion":
+      isLiveVideoDiscussion = true;
+  }
+  res.render("courses/course", {
+    isDiscussion,
+    isLiveDiscussion,
+    isLiveVideoDiscussion,
+  });
 });
 
 module.exports = router;
